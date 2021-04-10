e1b617dd889c77923baf7f65f5f0dfe68bef1d3a
Seon-Ae Hwang <70480480+seon-ae@users.noreply.github.com>
2021-04-11 01:29:30 +0900

    Update 2 on GitHub branch Only

--- main.py
+++ main.py
@@ -21,3 +21,6 @@ print("Hello World updates on Github branch")
 
 #adding new item in master branch
 print("Hello World Updates on Local Master Branch")
+
+#updates in main branch of GitHub
+print("Hello World updates on Github branch Only")
